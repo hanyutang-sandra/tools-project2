53c04158b960c94e2b13f3dd540fced7de046741
Hanyu <hanyutang.sandra@gmail.com>
2019-03-31 19:22:50 -0400

    Deployed

--- tools2/tools2/settings.py
+++ tools2/tools2/settings.py
@@ -20,14 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = '!k8-oofnbb-=!)z8k9tj2&5s%^+&syqht-rb0^*ph**xhr&@1b'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = '!k8-oofnbb-=!)z8k9tj2&5s%^+&syqht-rb0^*ph**xhr&@1b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-#ALLOWED_HOSTS = ['immense-wave-80157.herokuapp.com']
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['radiant-basin-41966.herokuapp.com']
+
 
 
 # Application definition
